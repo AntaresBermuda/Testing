e8c70c438748f0dca2935c9ead19422615c46d5d
AntaresBermuda <55585693+AntaresBermuda@users.noreply.github.com>
2023-11-08 15:32:13 -0400

    efe

--- app.py
+++ app.py
@@ -9,51 +9,47 @@ from dateutil.relativedelta import relativedelta
 import time
 
 
-if st.button("Run Simulation"):
+@st.cache(show_spinner=False)
+def generate_download_link():
+    # Code to generate the file you want to download
+    # This function will only be executed once, and the output will be cached
 
-    @st.cache(show_spinner=False)
-    def generate_download_link():
-        # Code to generate the file you want to download
-        # This function will only be executed once, and the output will be cached
+    # Replace the following code with your own logic to generate the file
+    file_contents = "Hello, this is the file content!"
 
-        # Replace the following code with your own logic to generate the file
-        file_contents = "Hello, this is the file content!"
+    return file_contents
 
-        return file_contents
+def main():
+    # Generate the download link
+    file_contents = generate_download_link()
 
-    def main():
-        # Generate the download link
-        file_contents = generate_download_link()
+    # Create a download button using st.download_button
+    if st.button("Download File"):
+        # When the button is clicked, download the file
+        # You can specify the file name and content type
+        st.download_button(
+            label="Click here to download",
+            data=file_contents,
+            file_name="file.txt",
+            mime="text/plain"
+        )
 
-        # Create a download button using st.download_button
-        if st.button("Download File"):
-            # When the button is clicked, download the file
-            # You can specify the file name and content type
-            st.download_button(
-                label="Click here to download",
-                data=file_contents,
-                file_name="file.txt",
-                mime="text/plain"
-            )
-
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
 
 
-'''
-slid = st.slider("slide me", min_value=0, max_value=10)
 
-if st.button("Run Simulation"):
+#slid = st.slider("slide me", min_value=0, max_value=10)
 
-    df = pd.DataFrame()
-    df["Nothing"] = [1,2,3]
+#if st.button("Run Simulation"):
 
-    st.markdown("Don't make me dissapear :(")
+#    df = pd.DataFrame()
+#    df["Nothing"] = [1,2,3]
 
-    def getdata(df):
-        return df.to_csv(index=False).encode('utf-8')
+#    st.markdown("Don't make me dissapear :(")
 
-    if st.download_button('Download Simulation', data=getdata(df), file_name='Simulation.csv'):
-        st.markdown("????")
+#    def getdata(df):
+#        return df.to_csv(index=False).encode('utf-8')
 
-'''
+#    if st.download_button('Download Simulation', data=getdata(df), file_name='Simulation.csv'):
+#        st.markdown("????")
